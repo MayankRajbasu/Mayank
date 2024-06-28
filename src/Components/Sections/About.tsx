e23e19d65269a9eb2accd1f06fa59f8cf74eff57
MAYANK RAJ <mayank.09raj@gmail.com>
2024-06-29 02:01:53 +0530

    media screen adjustments & scroll-snap-none

--- src/Components/Sections/About.tsx
+++ src/Components/Sections/About.tsx
@@ -1,8 +1,8 @@
 function About() {
   return (
-    <article className="flex justify-center gap-4 relative flex-wrap flex-col items-center w-full h-screen xl:px-64 lg:px-6 sm:px-20 xsm:px-6 sd:py-36 xsm:py-12 ">
+    <article className="flex justify-center gap-4 relative flex-wrap flex-col items-center w-full min-h-fit xl:px-64 lg:px-6 sm:px-20 xsm:px-6 sd:py-36 xsm:pb-12 xsm:pt-0 ">
       <div id="about-content" className="flex flex-col w-full mt-7 relative">
-        <h1 className="font-regular text-2xl flex font-semibold text-slate-300 text-start mb-3 before:text-lg before:font-medium after:bg-slate-600 after:xsm:w-36 after:msd:w-52 after:sd:w-64 ">About Me</h1>
+        <h1 className="font-regular text-2xl flex font-semibold text-slate-300 text-start mb-3 before:text-lg before:font-medium after:bg-slate-600 after:xsm:w-24 after:msd:w-52 after:sd:w-64 ">About Me</h1>
         <p className="relative font-regular pt-5 text-left text-sd">Hello! My Name is Mayank and I love to develop Website and work on ML models. Though I have had an encounter with basics of HTML & CSS back in 6th grade, but for me a complete new journey in web development began in 2021 and fast forward to today, I have developed various small and mid-scale scale projects acquiring great skills in HTML & CSS!.</p>
         <p className="relative font-regular pt-2 text-left text-sd">Talking about Machine learning, my journey began with an excitement to learn something very new and with building a simple classification model. Fast forward to today, I am able to develop prediction models based on classical and deep learning models.</p>
         <p className="relative font-regular pt-2 text-left text-sd">Here are a few technical skills I’ve been acquired along the way :</p>
