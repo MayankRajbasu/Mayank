09eb7884faad67f9d6c8df14221ed06eec40c59c
MAYANK RAJ <mayank.09raj@gmail.com>
2024-06-29 14:21:27 +0530

    Work section #start-head

--- src/Components/Sections/Project.tsx
+++ src/Components/Sections/Project.tsx
@@ -1,6 +1,10 @@
 function Project() {
   return (
-    <div>Project</div>
+    <article className="flex justify-center gap-4 relative flex-wrap flex-col items-center w-full h-screen xl:px-64 lg:px-6 sm:px-20 xsm:px-6 sd:py-36 xsm:py-11 ">
+      <div id="Project-content" className="flex flex-col w-full sm:mt-7 xsm:mt-2 relative">
+        <h1 className="font-regular sd:text-2xl xsm:text-[22px] flex font-semibold text-slate-300 text-start mb-3 before:text-lg before:font-medium after:bg-slate-600 after:msd:w-52 after:sd:w-64 after:xsm:w-14 after:xsm:mt-1">What I've built</h1>
+      </div>
+    </article>
   )
 }
 
