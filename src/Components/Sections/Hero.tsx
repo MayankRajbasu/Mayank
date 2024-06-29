a62e876f14fb466d7ff0fc50a38fd1fdaefd1d52
MAYANK RAJ <mayank.09raj@gmail.com>
2024-06-29 10:13:29 +0530

    adjusting sizes

--- src/Components/Sections/Hero.tsx
+++ src/Components/Sections/Hero.tsx
@@ -1,10 +1,10 @@
 function Hero() {
   return (
-    <article className="flex gap-5 lg:justify-between xsm:justify-center relative flex-wrap items-center w-full min-h-screen lg:px-36 xsm:px-6 xsm:py-12">
+    <article className="flex gap-5 lg:justify-between xsm:justify-center relative flex-wrap items-center w-full h-screen lg:px-36 xsm:px-6 xsm:py-12">
       <div id="content" className="flex flex-col">
-        <p className="font-regular text-teal-400 font-normal lg:text-lg xsm:text-base xsm:py-2">Hi, my name is</p>
-        <p className="font-hero lg:text-7xl text-slate-300 font-semibold xsm:text-5xl xsm:py-2">Mayank Raj.</p>
-        <p className="font-hero lg:text-6xl font-medium xsm:text-4xl xsm:font-semibold pb-2 tracking-wide">I build things for the web and ML.</p>
+        <p className="font-regular text-teal-400 font-normal lg:text-lg xsm:text-base xsm:py-1">Hi, my name is</p>
+        <p className="font-hero lg:text-7xl text-slate-300 font-semibold xsm:text-[36px] xsm:pb-1">Mayank Raj.</p>
+        <p className="font-hero lg:text-6xl font-medium xsm:text-[32px] xsm:font-semibold pb-2 tracking-wide">I build things for the web and ML.</p>
         <div className="flex gap-3">
           <div className="border-2 py-2 px-4 text-teal-400 border-teal-400 content-center font-regular hover:cursor-pointer hover:shadow-indigo-500/40 rounded">
             <a href="https://drive.google.com/file/d/1jWfDGNlKoOsC6RxrWdaXbXi5xRL872eK/view" target='_blank'>Resume</a>
@@ -14,7 +14,7 @@ function Hero() {
           </div>
         </div>
       </div>
-      <div id="img-content"  className="w-[300px] h-[300px] bg-[#64ffda] rounded hover:-translate-x-1 hover:-translate-y-1 relative ">
+      <div id="img-content"  className="sd:w-[300px] sd:h-[300px] xsm:w-[250px] xsm:[250px] bg-[#64ffda] rounded hover:-translate-x-1 hover:-translate-y-1 relative ">
         <div className="w-full h-full after:border-2 after:w-full after:h-full after:border-teal-400 after:block after:absolute after:top-3 after:left-3 after:-z-10 hover:after:translate-x-2 hover:after:translate-y-2 after:rounded">
           <img className="w-full h-full object-cover rounded grayscale contrast-100 mix-blend-multiply hover:grayscale-0 hover:mix-blend-normal " src="/Mayank/images/Mayank.jpg"></img>
         </div>
--- src/Components/Sections/Work.tsx
+++ src/Components/Sections/Work.tsx
@@ -1,19 +1,19 @@
 function Work() {
   return (
-    <article className="flex justify-center gap-4 relative flex-wrap flex-col items-center w-full min-h-screen xl:px-64 lg:px-6 sm:px-20 xsm:px-6 sd:py-36 xsm:pb-12 xsm:pt-0 ">
+    <article className="flex justify-center gap-4 relative flex-wrap flex-col items-center w-full h-screen xl:px-64 lg:px-6 sm:px-20 xsm:px-6 sd:py-36 xsm:pb-12 xsm:pt-0 ">
       <div id="work-content" className="flex flex-col w-full sm:mt-7 xsm:mt-2 relative">
-        <h1 className="font-regular text-2xl flex font-semibold text-slate-300 text-start mb-3 before:text-lg before:font-medium after:bg-slate-600 after:msd:w-52 after:sd:w-64 after:xsm:w-12">My Experience</h1>
-        <div className="relative font-regular pt-5 text-left text-sd">
-          <h2 className="font-hero font-medium text-xl" >Frontend Intern <span className="text-teal-400">@ ScienceOverse</span></h2>
-          <p className="font-regular text-sd font-light">Apr 2024 - May 2024</p>
+        <h1 className="font-regular sd:text-2xl xsm:text-[22px] flex font-semibold text-slate-300 text-start mb-3 before:text-lg before:font-medium after:bg-slate-600 after:msd:w-52 after:sd:w-64 after:xsm:w-14 after:xsm:mt-1">My Experience</h1>
+        <div className="relative font-regular sd:pt-5 xsm:pt-1 text-left sd:text-sd xsm:text-[12px]">
+          <h2 className="font-hero font-medium sd:text-xl xsm:text-[18px]" >Frontend Intern <span className="text-teal-400">@ ScienceOverse</span></h2>
+          <p className="font-regular sd:text-sd xsm:text-[16px] font-light">Apr 2024 - May 2024</p>
           <ul className="font-regular pt-2 pl-3 text-left">
             <li>Successfully developed multiple interactive and responsive webpages using React, Next.js, and Tailwind CSS,
               contributing to the overall user experience and functionality of the website.</li>
             <li>Identified, debugged, and resolved critical issues across the website, improving stability and performance,
               which led to a more efficient and user-friendly application.</li>
           </ul>
-          <h2 className="font-hero font-medium text-xl mt-4" >Data Science Intern <span className="text-teal-400">@ Let'sGrowMore</span></h2>
-          <p className="font-regular text-sd font-light">April 2024 - May 2024</p>
+          <h2 className="font-hero font-medium sd:text-xl xsm:text-[18px] mt-4" >Data Science Intern <span className="text-teal-400">@ Let'sGrowMore</span></h2>
+          <p className="font-regular sd:text-sd xsm:text-[16px] font-light">April 2024 - May 2024</p>
           <ul className="font-regular pt-2 pl-3 text-left">
             <li>Improved familiarity with ML and Decision Tree Models with practical use case. </li>
             <li>Engineered a simple solution to convert photos to pencil drawings using OpenCV and Python.</li>
