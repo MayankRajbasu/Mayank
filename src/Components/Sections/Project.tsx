2b2b0da65f9f4df7650dfea9e9812e5af80b7422
MAYANK RAJ <mayank.09raj@gmail.com>
2024-06-29 14:30:37 +0530

    renamed workflow

--- src/Components/Sections/Project.tsx
+++ src/Components/Sections/Project.tsx
@@ -1,7 +1,7 @@
 function Project() {
   return (
     <article className="flex justify-center gap-4 relative flex-wrap flex-col items-center w-full h-screen xl:px-64 lg:px-6 sm:px-20 xsm:px-6 sd:py-36 xsm:py-11 ">
-      <div id="Project-content" className="flex flex-col w-full sm:mt-7 xsm:mt-2 relative">
+      <div id="project-content" className="flex flex-col w-full sm:mt-7 xsm:mt-2 relative">
         <h1 className="font-regular sd:text-2xl xsm:text-[22px] flex font-semibold text-slate-300 text-start mb-3 before:text-lg before:font-medium after:bg-slate-600 after:msd:w-52 after:sd:w-64 after:xsm:w-14 after:xsm:mt-1">What I've built</h1>
       </div>
     </article>
