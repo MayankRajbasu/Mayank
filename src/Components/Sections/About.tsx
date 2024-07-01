bdb04ea29fd7077d96161b020cdd91faf9e86fcd
MAYANK RAJ <mayank.09raj@gmail.com>
2024-07-01 09:18:41 +0530

    Style update (letter-spacing)

--- src/Components/Sections/About.tsx
+++ src/Components/Sections/About.tsx
@@ -2,12 +2,12 @@ function About() {
   return (
     <article className="flex justify-center gap-4 relative flex-wrap flex-col items-center w-full h-screen xl:px-64 lg:px-6 sm:px-20 xsm:px-6 sd:py-36 xsm:py-11 ">
       <div id="about-content" className="flex flex-col w-full sd:mt-7 xsm:mt-2 relative">
-        <h1 className="font-regular sd:text-2xl xsm:text-[22px] flex font-semibold text-slate-300 text-start mb-3 before:text-[16px] sd:before:text-lg before:font-medium after:bg-slate-600 after:xsm:w-24 after:msd:w-52 after:sd:w-64 after:xsm:mt-1">About Me</h1>
-        <p className="relative font-regular pt-5 text-left sd:text-sd xsm:text-[12px]">Hello! My Name is Mayank and I love to develop Website and work on ML models. Though I have had an encounter with basics of HTML & CSS back in 6th grade, but for me a complete new journey in web development began in 2021 and fast forward to today, I have developed various small and mid-scale scale projects acquiring great skills in HTML & CSS!.</p>
-        <p className="relative font-regular pt-2 text-left sd:text-sd xsm:text-[12px]">Talking about Machine learning, my journey began with an excitement to learn something very new and with building a simple classification model. Fast forward to today, I am able to develop prediction models based on classical and deep learning models.</p>
-        <p className="relative font-regular pt-2 text-left sd:text-sd xsm:text-[12px]">Here are a few technical skills I’ve been acquired along the way :</p>
+        <h1 className="font-regular sd:text-2xl xsm:text-[22px] flex font-semibold text-slate-300 text-start tracking-tight mb-3 before:text-[16px] sd:before:text-lg before:font-medium after:bg-slate-600 after:xsm:w-24 after:msd:w-52 after:sd:w-64 after:xsm:mt-1">About Me</h1>
+        <p className="relative font-regular pt-5 text-left tracking-tight sd:text-sd xsm:text-[12px]">Hello! My Name is Mayank and I love to develop Website and work on ML models. Though I have had an encounter with basics of HTML & CSS back in 6th grade, but for me a complete new journey in web development began in 2021 and fast forward to today, I have developed various small and mid-scale scale projects acquiring great skills in HTML & CSS!.</p>
+        <p className="relative font-regular pt-2 text-left tracking-tight sd:text-sd xsm:text-[12px]">Talking about Machine learning, my journey began with an excitement to learn something very new and with building a simple classification model. Fast forward to today, I am able to develop prediction models based on classical and deep learning models.</p>
+        <p className="relative font-regular pt-2 text-left tracking-tight sd:text-sd xsm:text-[12px]">Here are a few technical skills I’ve been acquired along the way :</p>
       </div>
-      <ul id="skills-content" className="relative grid xsm:grid-cols-3 lg:grid-cols-5 w-full text-start py-4 sm:px-2 xsm:px-0 sd:text-sd xsm:text-[12px]">
+      <ul id="skills-content" className="relative grid tracking-tight xsm:grid-cols-3 lg:grid-cols-5 w-full text-start py-4 sm:px-2 xsm:px-0 sd:text-sd xsm:text-[12px]">
         <li className="font-regular">Javascript</li>
         <li className="font-regular">Python</li>
         <li className="font-regular">Java</li>
--- src/Components/Sections/Work.tsx
+++ src/Components/Sections/Work.tsx
@@ -4,17 +4,17 @@ function Work() {
       <div id="work-content" className="flex flex-col w-full sm:mt-7 xsm:mt-0 relative">
         <h1 className="font-regular sd:text-2xl xsm:text-[22px] flex font-semibold text-slate-300 text-start mb-3 before:text-lg before:font-medium after:bg-slate-600 after:msd:w-52 after:sd:w-64 after:xsm:w-14 after:xsm:mt-1">My Experience</h1>
         <div className="relative font-regular sd:pt-5 xsm:pt-1 text-left sd:text-sd xsm:text-[12px]">
-          <h2 className="font-medium sd:text-xl xsm:text-[15px]" >Frontend Intern <span className="text-teal-400">@ ScienceOverse</span></h2>
+          <h2 className="font-medium sd:text-xl xsm:text-[15px] tracking-tighter" >Frontend Intern <span className="text-teal-400">@ ScienceOverse</span></h2>
           <p className="sd:text-sd xsm:text-[14px] font-light">Apr 2024 - May 2024</p>
-          <ul className="pt-2 pl-3 text-left">
+          <ul className="pt-2 pl-3 text-left tracking-tight">
             <li>Successfully developed multiple interactive and responsive webpages using React, Next.js, and Tailwind CSS,
               contributing to the overall user experience and functionality of the website.</li>
             <li>Identified, debugged, and resolved critical issues across the website, improving stability and performance,
               which led to a more efficient and user-friendly application.</li>
           </ul>
-          <h2 className="font-medium sd:text-xl xsm:text-[15px] mt-4" >Data Science Intern <span className="text-teal-400">@ Let'sGrowMore</span></h2>
+          <h2 className="font-medium sd:text-xl xsm:text-[15px] mt-4 tracking-tighter" >Data Science Intern <span className="text-teal-400">@ Let'sGrowMore</span></h2>
           <p className="sd:text-sd xsm:text-[14px] font-light">April 2024 - May 2024</p>
-          <ul className="pt-2 pl-3 text-left">
+          <ul className="pt-2 pl-3 text-left tracking-tight">
             <li>Improved familiarity with ML and Decision Tree Models with practical use case.</li>
             <li>Engineered a simple solution to convert photos to pencil drawings using OpenCV and Python.</li>
           </ul>
