cd73b2d9f151124b3ad8bda84503a1bb7b9fe462
MAYANK RAJ <mayank.09raj@gmail.com>
2024-07-03 14:41:13 +0530

    style update (contact #4 -fonts)

--- src/Components/Sections/Contact.tsx
+++ src/Components/Sections/Contact.tsx
@@ -4,12 +4,12 @@ function Contact() {
       <div id="contact-content" className="flex flex-col items-center w-full sm:mt-7 xsm:mt-2 relative">
         <h1 className="font-regular sd:text-[22px] xsm:text-[16px] flex font-medium text-teal-400 mb-3 before:sd:text-lg  before:font-medium after:bg-slate-600">What's next?</h1>
         <p className="font-hero text-slate-300/75 xl:text-[52px] sd:text-[48px] xsm:text-[36px] font-bold tracking-wide">Get In Touch</p>
-        <p className="font-regular xl:px-72 mb-6 pb-3 text-center">I'm currently open for any new opportunities in field of Fullstack and Machine learning developement. Whether you have any querry or just wanna say hi, my inbox is open. I'll try my best to get back to you! </p>
-        <div className="border-2 py-2 px-4 text-teal-400 border-teal-400 content-center font-regular hover:cursor-pointer rounded">
+        <p className="font-regular xl:px-72 mb-6 pb-3 text-center text-[14px]">I'm currently open for any new opportunities in field of Fullstack and Machine learning developement. Whether you have any querry or just wanna say hi, my inbox is open. I'll try my best to get back to you! </p>
+        <div className="border-2 py-2 px-4 text-teal-400 border-teal-400 content-center font-regular hover:cursor-pointer rounded text-[14px]">
           <a href="mailto:mayank.09raj@gmail.com">Say Hello!</a>
         </div>
       </div>
-      <div id="footer" className="relative xsm:top-24 sm:top-36 xl:top-52">
+      <div id="footer" className="relative xsm:top-24 sm:top- xl:top-52">
         <div className="flex text-slate-300 justify-center gap-4">
           <a href="https://github.com/MayankRajbasu" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
             strokeLinejoin="round" className="feather feather-github hover:-translate-x-1 hover:-translate-y-1"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
